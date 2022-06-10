b49151f689d46aa642a2b34114a70f14a20a6c49
Francisca Nunes <franciscaribeironunes@gmail.com>
2022-06-11 00:51:40 +0100

    Create institution form - frontend

--- frontend/src/views/institution/RegisterInstitutionView.vue
+++ frontend/src/views/institution/RegisterInstitutionView.vue
@@ -1,17 +1,92 @@
 <template>
-  <div class="container">Registar Instituicao</div>
+  <div class="container">
+    <v-form ref="form" lazy-validation>
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        label="Name"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        label="E-mail"
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="nif"
+        label="NIF"
+        required
+        @input="$v.nif.$touch()"
+        @blur="$v.nif.$touch()"
+      ></v-text-field>
+
+      <v-file-input
+        counter
+        multiple
+        show-size
+        truncate-length="7"
+      ></v-file-input>
+
+      <v-btn class="mr-4" @click="submit"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import RegisterUser from '@/models/institution/Institution';
+import Institution from '@/models/institution/Institution';
+import RemoteServices from '@/services/RemoteServices';
 
 @Component({
   components: {},
 })
 export default class RegisterInstitutionView extends Vue {
-  async created() {}
+  name: string = '';
+  email: string = '';
+  nif: string = '';
+  async submit() {
+    await this.$store.dispatch('loading');
+    try {
+      let institution: Institution = await RemoteServices.RegisterUser({
+        name: this.name,
+        email: this.email,
+      });
+      await this.$store.dispatch('institution', institution);
+      await this.$router.push({
+        name: 'solve-quiz',
+      });
+    } catch (error) {
+      await this.$store.dispatch('error', error);
+    }
+    await this.$store.dispatch('clearLoading');
+  }
+
+  clear() {
+    this.name = '';
+    this.email = '';
+    this.nif = '';
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  margin-top: 2rem !important;
+  padding: 3rem !important;
+  width: 60%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: stretch;
+  align-content: center;
+  background-color: rgba(255, 255, 255);
+}
+</style>
