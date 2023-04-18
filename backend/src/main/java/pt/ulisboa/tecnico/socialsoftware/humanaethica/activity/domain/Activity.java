9e0eaac8846b0a88fbc1bcb080d009399681e144
ruipmfs <rui.pmfs@gmail.com>
2023-04-18 17:27:51 +0100

    feat(domain): added package activity and its current domain and dto

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activity/domain/Activity.java
@@ -0,0 +1,84 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain;
+
+import jakarta.persistence.*;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Member;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "activity")
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String region;
+    private boolean state;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "activity", orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Volunteer> volunteers = new ArrayList<>();
+    /*
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "institution", fetch = FetchType.EAGER, orphanRemoval = true)
+    private Theme theme;
+    */
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    public Activity() {
+    }
+    public Activity(String name, String region) {
+        setName(name);
+        setRegion(region);
+        setState(true);
+        setCreationDate(DateHandler.now());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public boolean isActive() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public void delete() {
+        setState(false);
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List<Volunteer> getVolunteers() {
+        return volunteers;
+    }
+
+}
---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activity/dto/ActivityDto.java
@@ -0,0 +1,68 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ActivityDto {
+    private Integer id;
+
+    private String name;
+
+    private String region;
+
+    private boolean state;
+
+    private String creationDate;
+
+    private List<Volunteer> volunteers;
+
+    public ActivityDto(){
+    }
+
+    public ActivityDto(Activity activity){
+        setId(activity.getId());
+        setName(activity.getName());
+        setRegion(activity.getRegion());
+        setState(activity.isActive());
+        setCreationDate(DateHandler.toISOString(activity.getCreationDate()));
+        setVolunteers(activity.getVolunteers());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() { return region; }
+
+    public void setRegion(String region) { this.region = region; }
+
+    public boolean isActive() { return state; }
+
+    public void setState(boolean state) { this.state = state; }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setVolunteers(List<Volunteer> volunteers) { this.volunteers = volunteers; }
+}
