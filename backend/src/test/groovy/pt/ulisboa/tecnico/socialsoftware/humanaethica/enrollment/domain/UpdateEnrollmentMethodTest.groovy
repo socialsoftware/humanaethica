ce8fd7cef41fb88e24514cba7c1b1a2da50a9bca
André Pereira <andre.aguiar.pereira@tecnico.ulisboa.com>
2024-05-17 23:32:07 +0100

    feat(domain test): updated UpdateEnrollmentMethodTest
    related issues: #36

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/UpdateEnrollmentMethodTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/UpdateEnrollmentMethodTest.groovy
@@ -27,7 +27,7 @@ class UpdateEnrollmentMethodTest extends SpockTest {
         given:
         enrollmentDtoOne = new EnrollmentDto()
         enrollmentDtoOne.motivation = ENROLLMENT_MOTIVATION_1
-        enrollmentDtoOne.enrollmentDateTime = DateHandler.toISOString(ONE_DAY_AGO)
+        enrollmentDtoOne.enrollmentDateTime = DateHandler.toISOString(TWO_DAYS_AGO)
 
         and: "enrollment"
         otherEnrollment.getMotivation() >> ENROLLMENT_MOTIVATION_2 
@@ -68,6 +68,21 @@ class UpdateEnrollmentMethodTest extends SpockTest {
         null       || ErrorMessage.ENROLLMENT_REQUIRES_MOTIVATION
         "   "      || ErrorMessage.ENROLLMENT_REQUIRES_MOTIVATION
         "< 10"     || ErrorMessage.ENROLLMENT_REQUIRES_MOTIVATION
+    }
+
+
+    def "try to update enrollment after deadline"() {
+        given:
+        activity.getApplicationDeadline() >> ONE_DAY_AGO
+        and:
+        enrolmentDtoTwo.motivation = ENROLLMENT_MOTIVATION_2
+
+        when:
+        enrollment.edit(enrollmentDtoTwo)
+
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == ErrorMessage.ENROLLMENT_AFTER_DEADLINE
     }
 
 
