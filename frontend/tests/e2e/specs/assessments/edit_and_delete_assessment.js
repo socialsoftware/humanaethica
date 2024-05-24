9b5cac0d639f4b8e158d93b5def70e00ed06dd38
Pedro Gregório <joaogregoriotecnico@gmail.com>
2024-05-24 11:39:14 -0700

    feat: test delete assessment (e2e)

--- frontend/tests/e2e/specs/assessments/edit_and_delete_assessment.js
+++ frontend/tests/e2e/specs/assessments/edit_and_delete_assessment.js
@@ -13,17 +13,12 @@ describe('Edit Assessment', () => {
   it('edit assessment', () => {
     const UPDATE = "This is an update.";
 
-    //cy.intercept('GET', '/enrollments/volunteer').as('enrollments');
-    //cy.intercept('GET', '/participations/volunteer').as('participations');
     cy.intercept('GET', '/assessments/volunteer').as('assessments');
     cy.intercept('PUT', '/assessments/1').as('review');
-    //cy.intercept('GET', '/institutions/1/assessments').as('reviews');
 
     // volunteer updates assessment
     cy.demoVolunteerLogin();
     cy.get('[data-cy="volunteerAssessments"]').click();
-    //cy.wait('@enrollments');
-    //cy.wait('@participations');
     cy.wait('@assessments');
 
     // verify that assessment has "DEMO INSTITUTION-2" is in row 0
@@ -52,6 +47,41 @@ describe('Edit Assessment', () => {
       .children()
       .eq(1)
       .should('contain', UPDATE);
+
+    cy.logout();
+  });
+
+  it('delete assessment', () => {
+    cy.intercept('GET', '/assessments/volunteer').as('assessments');
+    cy.intercept('DELETE', '/assessments/1').as('delete');
+
+    // volunteer updates assessment
+    cy.demoVolunteerLogin();
+    cy.get('[data-cy="volunteerAssessments"]').click();
+    cy.wait('@assessments');
+
+    // verify that assessment has "DEMO INSTITUTION-2" is in row 0
+    cy.get('[data-cy="volunteerAssessmentsTable"] tbody tr')
+      .should('have.length', 1)
+      .eq(0)
+      .children()
+      .eq(0)
+      .should('contain', "DEMO INSTITUTION-2")
+
+    // delete assessment
+    cy.get('[data-cy="volunteerAssessmentsTable"] tbody tr')
+      .eq(0)
+      .find('[data-cy="deleteAssessmentButton"]')
+      .click();
+    cy.wait('@delete');
+
+    // check review
+    cy.get('[data-cy="volunteerAssessmentsTable"] tbody tr')
+      .should('have.length', 1)
+      .eq(0)
+      .children()
+      .eq(0)
+      .should('contain', 'No data available');
 
     cy.logout();
   });
