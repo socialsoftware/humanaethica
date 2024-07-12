50c64d6fd0fa4e48fc2c59575d5ea311ac0055ad
joaogreg <92681779+joaogreg@users.noreply.github.com>
2024-07-12 17:46:23 +0100

    Member/Admin suspends activity with justification (Frontend) (#133)
    * feat: admin and member suspend activity dialog
    
    * feat: e2e test member suspends activity
    
    * feat: e2e test admin suspends activity
    
    * fix: admin activity view
    
    ---------
    
    Co-authored-by: Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>

--- frontend/tests/e2e/specs/member/member.js
+++ frontend/tests/e2e/specs/member/member.js
@@ -1,10 +1,15 @@
 describe('Volunteer', () => {
   beforeEach(() => {
+    cy.deleteAllButArs()
+    cy.createDemoEntities();
+    cy.createDatabaseInfoForEnrollments()
+
     cy.demoMemberLogin()
   });
 
   afterEach(() => {
     cy.logout();
+    cy.deleteAllButArs()
   });
 
   it('close', () => {
@@ -16,5 +21,26 @@ describe('Volunteer', () => {
 
     cy.get('[data-cy="institution"]').click();
     cy.get('[data-cy="activities"]').click();
+  });
+
+  it('suspend activity', () => {
+    const JUSTIFICATION = 'This activity will be suspended for testing.';
+
+    cy.intercept('GET', '/users/2/getInstitution').as('activities')
+
+    // List activities.
+    cy.get('[data-cy="institution"]').click()
+    cy.get('[data-cy="activities"]').click()
+    cy.wait('@activities')
+
+    // Suspend first activity.
+    cy.get('[data-cy="suspendButton"]').first().click()
+    cy.get('[data-cy="suspensionReasonInput"]').type(JUSTIFICATION)
+    cy.get('[data-cy="suspendActivity"]').click()
+
+    // Check if first activity is suspended.
+    cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+      .should('have.length', 3)
+      .should('contain', "SUSPENDED")
   });
 });
