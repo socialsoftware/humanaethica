6e0f004ca0ea4f41ccc558b844f15fa86154d67a
mati-soda <matilde.simoes@tecnico.ulisboa.pt>
2025-04-03 18:21:46 +0100

    refactor: added comments to code #112

--- frontend/tests/e2e/specs/institutionProfile/institutionProfile.js
+++ frontend/tests/e2e/specs/institutionProfile/institutionProfile.js
@@ -11,9 +11,9 @@ describe('InstitutionProfile', () => {
 
   it('create institution profile', () => {
     const SHORT_DESCRIPTION = 'This is a short description.';
+    const REVIEW = 'Participation numa cãorrida da Demo Institution. O evento foi muito bem organizado, parabéns!'
 
     cy.intercept('POST', '/profile/institution').as('createInstitutionProfile');
-
     cy.intercept('GET', '/profile/institution/1').as('institutionProfile');
     cy.intercept('GET', '/institutions/1/assessments').as('assessments');
     cy.intercept('GET', '/users/*/getInstitution').as('getInstitution');
@@ -27,33 +27,32 @@ describe('InstitutionProfile', () => {
     cy.wait('@assessments');
 
     cy.get('[data-cy="createInstitutionPButton"]').click();
-
     cy.get('[data-cy="shortDescriptionInput"]').type(SHORT_DESCRIPTION);
-    // verify that assessment with review "muito bom" is in row 0
+    // verify that assessment with review is in row 0
     cy.get('[data-cy="institutionAssessmentsTable"] tbody tr')
       .should('have.length', 2)
       .eq(0)
       .children()
       .eq(2)
-      .should('contain', "Participation numa cãorrida da Demo Institution. O evento foi muito bem organizado, parabéns!")
+      .should('contain', REVIEW)
     // open write assessment dialog
     cy.get('[data-cy="institutionAssessmentsTable"] tbody tr')
-    .eq(0) // Pega a primeira linha
-    .find('td .v-simple-checkbox')
-    .click();
-    // write review
-  
+      .eq(0) // click on first assessment
+      .find('td .v-simple-checkbox')
+      .click();
+
     cy.get('[data-cy="saveInstitutionProfile"]').click();
     cy.wait('@createInstitutionProfile');
     cy.wait('@institutionProfile');
+
     cy.get('[data-cy="numAssessments"]') 
-    .should('contain', '2');
+      .should('contain', '2');
     cy.get('[data-cy="institutionProfileAssessmentsTable"] tbody tr')
-    .should('have.length', 1)
-    .eq(0)
-    .children()
-    .eq(1)
-    .should('contain', "Participation numa cãorrida da Demo Institution. O evento foi muito bem organizado, parabéns!");
+      .should('have.length', 1)
+      .eq(0)
+      .children()
+      .eq(1)
+      .should('contain', REVIEW);
 
     cy.logout();
 
@@ -62,7 +61,7 @@ describe('InstitutionProfile', () => {
     cy.get('[data-cy="view-profiles"]').click();
     cy.wait('@institutionProfileList');
 
-    // verify that assessment has "DEMO INSTITUTION-2" is in row 0
+    // verify that assessment has "DEMO INSTITUTION" is in row 0
     cy.get('[data-cy="institutionProfilesTable"] tbody tr')
       .should('have.length', 1)
       .eq(0)
