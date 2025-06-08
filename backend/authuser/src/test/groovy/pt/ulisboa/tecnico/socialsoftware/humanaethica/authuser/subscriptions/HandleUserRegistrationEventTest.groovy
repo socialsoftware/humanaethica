3c3f88fade0719464b2df23ccf6cba8b809e17e0
Daniel Costa <danielpcosta20@gmail.com>
2025-06-08 18:06:40 +0100

    feat(authuser): add tests for user registration and deletion events

---
+++ backend/authuser/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/authuser/subscriptions/HandleUserRegistrationEventTest.groovy
@@ -0,0 +1,69 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.authuser.subscriptions
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.authuser.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.authuser.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.dtos.auth.Type
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.dtos.user.RegisterUserDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.events.user.UserRegisteredEvent
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.dtos.user.Role
+
+@DataJpaTest
+class HandleUserRegistrationEventTest extends SpockTest {
+
+
+    @Autowired
+    AuthUserEventListener listener
+    def dto
+    def event
+
+    def setup() {
+        dto = new RegisterUserDto()
+        dto.setUsername(USER_1_USERNAME)
+        dto.setEmail(USER_1_EMAIL)
+        dto.setRole(Role.VOLUNTEER)
+        dto.setName(USER_1_NAME)
+        event = new UserRegisteredEvent(dto,1, Type.NORMAL, dto.getRole(), dto.getName())
+    }
+
+    def "directly creates auth user successfully"() {
+        given: "a valid registration event"
+
+
+        when: "the listener handles the event directly"
+        listener.hanldeUserRegistationEvent(event)
+
+        then: "the user is persisted"
+        authUserRepository.count() == 1
+        def result = authUserRepository.findAuthUserByUsername(USER_1_USERNAME).get()
+        result.username == USER_1_USERNAME
+        result.email == USER_1_EMAIL
+        result.role.name() == Role.VOLUNTEER.name()
+        result.userId != null
+    }
+
+
+    def "handleUserRegistrationEvent: is correctly subscribed and triggered by event bus"() {
+        given:
+
+
+        when:
+        userService.registerUser(dto, null)
+
+        then:
+        def result = authUserRepository.findAuthUserByUsername(USER_1_USERNAME).get()
+        result.username == USER_1_USERNAME
+        result.email == USER_1_EMAIL
+        result.role.name() == Role.VOLUNTEER.name()
+        result.userId != null
+    }
+
+
+
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+
+}
---
+++ backend/authuser/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/authuser/subscriptions/OnUserDeletedTest.groovy
@@ -0,0 +1,63 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.authuser.subscriptions
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.authuser.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.authuser.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.dtos.user.RegisterUserDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.dtos.user.Role
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.common.events.user.UserDeletedEvent
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.monolithic.user.dto.UserDto
+
+
+@DataJpaTest
+class OnUserDeletedTest extends SpockTest{
+
+
+    @Autowired
+    AuthUserEventListener listener
+    def dto
+    UserDto user
+
+    def setup() {
+        dto = new RegisterUserDto()
+        dto.setUsername(USER_1_USERNAME)
+        dto.setEmail(USER_1_EMAIL)
+        dto.setRole(Role.VOLUNTEER)
+        dto.setName(USER_1_NAME)
+        user = userService.registerUser(dto, null)
+    }
+
+    def "onUserDeleted: directly disables the user"() {
+        given: "an event for existing user"
+        def event = new UserDeletedEvent(user.getId())
+
+        when:
+        listener.onUserDeleted(event)
+
+        then:
+        def result = authUserRepository.findAuthUserByUsername(USER_1_USERNAME).get()
+        !result.active
+    }
+
+    def "onUserDeleted: disables user when triggered via subscription"() {
+        given: "register listener"
+
+
+        when:
+        userService.deleteUser(user.getId())
+
+        then:
+        def result = authUserRepository.findAuthUserByUsername(USER_1_USERNAME).get()
+        !result.active
+    }
+
+
+
+
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+
+}
