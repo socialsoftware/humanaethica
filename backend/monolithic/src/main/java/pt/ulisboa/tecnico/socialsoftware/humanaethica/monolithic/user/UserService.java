4f99d1e78d1919154ef858dee3661e42fc1b526b
Daniel Costa <danielpcosta20@gmail.com>
2025-09-24 16:49:39 +0100

    refactor(user): add username existence check in createVolunteer, createMember, and createAdmin methods

--- backend/monolithic/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/monolithic/user/UserService.java
+++ backend/monolithic/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/monolithic/user/UserService.java
@@ -144,18 +144,27 @@ public class UserService {
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
     public Integer createVolunteer(String name, String username, String email,  State state){
+        if (userRepository.findUserByUsername(username).isPresent()) {
+            throw new HEException(USERNAME_ALREADY_EXIST, username);
+        }
         Volunteer volunteer = new Volunteer(name, username, email, state);
         return userRepository.save(volunteer).getId();
     }
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
     public Integer createMember(String name, String username, String email, State state) {
+        if (userRepository.findUserByUsername(username).isPresent()) {
+            throw new HEException(USERNAME_ALREADY_EXIST, username);
+        }
         Member member = new Member(name, username, email, institutionService.getDemoInstitution(), state);
         return userRepository.save(member).getId();
     }
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
     public Integer createAdmin(String name, String username, String email, State state) {
+        if (userRepository.findUserByUsername(username).isPresent()) {
+            throw new HEException(USERNAME_ALREADY_EXIST, username);
+        }
         Admin admin = new Admin(name, username, email, state);
         return userRepository.save(admin).getId();
     }
